3fab7409df04ec232e7db34964e960895b456c76
Rafael Amaral <zorf310@gmail.com>
2025-10-21 15:26:47 -0300

    Exercícios 8, 9, 10 e 11

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil' // lembre que "==" é o sinal de igual.
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log(`Você é Estrangeiro!`)
+} else {
+    console.log(`Você é Brasileiro!`)
+}
