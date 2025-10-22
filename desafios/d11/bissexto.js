5e14634543d623c642dc1444536b6bc656e2dbcd
Rafael Amaral <zorf310@gmail.com>
2025-10-22 11:28:43 -0300

    Desafios 11, 12 e 13

---
+++ desafios/d11/bissexto.js
@@ -0,0 +1,8 @@
+var ano = 2035 //Atribua o ano que deseja verificar na variável. Em seguida "rode" o código com F8.
+console.log (`Analisando o ano de ${ano}`)
+if(ano%4 == 0){
+    console.log(`O ano de ${ano} é Bissexto!`)
+}
+else{
+    console.log(`O ano de ${ano} NÃO é Bissexto!`)
+}
