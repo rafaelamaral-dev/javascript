0093b42ca5a3a133650b950ee956afc3e353c885
Rafael Amaral <zorf310@gmail.com>
2025-10-27 14:43:36 -0300

    Aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 6)
+*/
+
+//Esta é uma estrutura de repetição com teste lógico no final.
+
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}
+
+
+//Esta é uma estrutura de repetição com teste lógico no início (a mais encontrada nos programas).
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
